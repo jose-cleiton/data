d1892a437e47fb8fcbf1330e1f8c11b93b53c627
José Cleiton C. Santos <cleitons835@gmail.com>
2022-12-27 11:37:34 -0300

    Create getSpeciesByIds.go

---
+++ getSpeciesByIds.go
@@ -0,0 +1,20 @@
+package data
+
+func GetSpeciesByIds(zoo Zoo, ids ...string) []Species {
+  // Declare uma variável para armazenar as espécies encontradas
+  var species []Species
+
+  // Percorra a lista de espécies do zoológico
+  for _, s := range zoo.Species {
+    // Verifique se o ID da espécie atual está presente na lista de IDs fornecidos
+    for _, id := range ids {
+      if s.ID == id {
+        // Se estiver, adicione a espécie ao array de espécies encontradas
+        species = append(species, s)
+      }
+    }
+  }
+
+  // Retorne o array de espécies encontradas
+  return species
+}
